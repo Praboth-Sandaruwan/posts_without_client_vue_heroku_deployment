bc2a4e383c2eab1bb79e31e9237e24d2af649903
Praboth Sandaruwan <it22148254@my.sliit.lk>
2025-10-21 11:07:26 +0530

    make it better

--- client/src/services/PostService.js
+++ client/src/services/PostService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from 'axios'
 
 const url = '/api/posts/'
@@ -49,55 +48,3 @@ class PostService {
 }
 
 export default PostService
-=======
-import axios from 'axios'
-
-const url = '/api/posts/'
-
-class PostService {
-  //Get all posts
-  static async getPosts() {
-    try {
-      const res = await axios.get(url)
-      const data = res.data
-      return data.map((post) => ({
-        ...post,
-        createdAt: new Date(post.createdAt),
-      }))
-    } catch (e) {
-      // statements
-      console.dir(e)
-      throw e
-    }
-  }
-
-  //Create a post
-  static async createPost(text) {
-    try {
-      const res = await axios.post(url, { text })
-      console.log({ ...res, FeMessage: 'Post created Successfully !!!' })
-      return res.data
-    } catch (e) {
-      // statements
-      console.dir(e)
-      throw e
-    }
-  }
-
-  //Delete a Post by ID
-  static async deleteById(id) {
-    try {
-      //console.log(`${url}${id}`)
-      const res = await axios.delete(`${url}${id}`)
-      console.log({ ...res, FeMessage: 'Post with ID : ' + id + ' deleted Successfully !!!' })
-      return res.data
-    } catch (e) {
-      // statements
-      console.dir(e)
-      throw e
-    }
-  }
-}
-
-export default PostService
->>>>>>> c5b9b17f34dc8932268447a539d2f0688d90c2ed
