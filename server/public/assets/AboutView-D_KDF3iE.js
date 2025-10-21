eec29cca079f04f13600c0e8bed482bbb994142c
Praboth Sandaruwan <it22148254@my.sliit.lk>
2025-10-21 09:36:41 +0530

    stt hr

---
+++ server/public/assets/AboutView-D_KDF3iE.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index-YAH0appx.js";import{g as t,j as s,q as a}from"./vendor-Bb6b9wbV.js";const r={},n={class:"about"};function c(i,e){return a(),t("div",n,[...e[0]||(e[0]=[s("h1",null,"This is an about page",-1)])])}const l=o(r,[["render",c]]);export{l as default};
